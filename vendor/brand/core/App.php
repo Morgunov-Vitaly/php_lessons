0e889f051185aa199adb44396e32f8e4cba1bae0
Morgunov-Vitaly <morgunov.vitaly@gmail.com>
2018-06-13 11:57:28 +0300

    Урок 1 Пространство имен namespace

---
+++ vendor/brand/core/App.php
@@ -0,0 +1,14 @@
+<?php
+namespace brand\core;
+
+/**
+ * Description of App
+ *
+ * @author Моргунов Виталий
+ */
+class App {
+    public function __construct() {
+        echo 'Выполнен метод __construct класса App в папке vendor/brand/core/';
+        echo "<br>";
+    }
+}
