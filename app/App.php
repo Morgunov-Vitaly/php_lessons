cb5b66b6ee84d593d7db314adda176d8f6375ba4
Morgunov-Vitaly <morgunov.vitaly@gmail.com>
2018-06-13 12:00:38 +0300

    Урок 1 Пространство имен namespace

---
+++ app/App.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app;
+/**
+ * Description of App
+ *
+ * @author Моргунов Виталий
+ */
+class App {
+    public function __construct() {
+        echo "Выполнен метод __construct класса App в папке app";
+        echo "<br>";
+        
+    }
+}
